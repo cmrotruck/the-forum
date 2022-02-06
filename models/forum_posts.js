0ac608196b966a71cabec05ef90015166c433f7b
cmrotruck <crotruck@gmail.com>
2022-02-06 17:54:00 -0500

    added forum_group model and updated index model

--- models/forum_posts.js
+++ models/forum_posts.js
@@ -37,7 +37,7 @@ Post.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: "Post",
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 // import all models
-const Post = require("./forum_post");
-const User = require("./forum_user");
-const Group = require("./forum_group");
-const Comment = require("./forum_comment");
+const Post = require("./forum_posts");
+const User = require("./forum_users");
+const Group = require("./forum_groups");
+const Comment = require("./forum_comments");
 
 // create associations
+
+//export all models
 module.exports = { User, Post, Group, Comment };
