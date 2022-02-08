9e124595c380d6be8d868e3ad72a8b3b19be62c0
cmrotruck <crotruck@gmail.com>
2022-02-08 07:03:01 -0500

    added table associations

--- models/index.js
+++ models/index.js
@@ -9,5 +9,33 @@ Group.hasMany(Post, {
   foreignKey: "group_id",
 });
 
+Post.belongsTo(Group, {
+  foreignKey: "group_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
 //export all models
 module.exports = { User, Post, Group, Comment };
