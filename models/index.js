3053f1341839fd0b97f7dc57f8b23c373145cd6f
cmrotruck <crotruck@gmail.com>
2022-02-08 07:07:02 -0500

    added notes to associations

--- models/index.js
+++ models/index.js
@@ -5,22 +5,26 @@ const Group = require("./Group");
 const Comment = require("./Comment");
 
 // create associations
+
+//group associations
 Group.hasMany(Post, {
   foreignKey: "group_id",
 });
 
+//post associations
 Post.belongsTo(Group, {
   foreignKey: "group_id",
-});
-
-User.hasMany(Post, {
-  foreignKey: "user_id",
 });
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+//comment associations
 Comment.belongsTo(User, {
   foreignKey: "user_id",
 });
@@ -29,12 +33,13 @@ Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
+//user associations
 User.hasMany(Comment, {
   foreignKey: "user_id",
 });
 
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
+User.hasMany(Post, {
+  foreignKey: "user_id",
 });
 
 //export all models
