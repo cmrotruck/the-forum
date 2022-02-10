e7ddaad37535e1e0c1d93afab4c15aa88f69993a
Debbie Hall <debleehall15@gmail.com>
2022-02-10 10:34:45 -0500

    homepage displaying when program is running

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,9 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment, Group } = require("../models");
 
+// handlebars set up
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
 module.exports = router;
