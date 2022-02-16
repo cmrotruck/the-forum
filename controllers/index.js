df813eeaf3d037c31a14172e7b2ee9587cee876d
Logand307 <92386527+Logand307@users.noreply.github.com>
2022-02-15 20:07:38 -0500

    Revert "Debbie"

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,9 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
-router.use('/dashboard', dashboardRoutes);
 
 router.get('/login', (req, res) => {
   res.render('login');
