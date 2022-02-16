ed0a33df0dafc8185dde19c82af102df22c19bdd
cmrotruck <crotruck@gmail.com>
2022-02-16 00:01:30 -0500

    added logout button

--- controllers/api/group-routes.js
+++ controllers/api/group-routes.js
@@ -1,46 +1,46 @@
-const router = require('express').Router();
-const { Group } = require('../../models');
+const router = require("express").Router();
+const { Group, Post } = require("../../models");
 // const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-    Group.findAll()
-      .then(dbGroupData => res.json(dbGroupData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.get("/", (req, res) => {
+  Group.findAll()
+    .then((dbGroupData) => res.json(dbGroupData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-  router.get('/:id', (req, res) => {
-    Group.findOne({
-      where: {
-        id: req.params.id,
+router.get("/:id", (req, res) => {
+  Post.findAll({
+    where: {
+      group_id: req.params.id,
+    },
+  })
+    .then((dbGroupData) => {
+      if (!dbGroupData) {
+        res
+          .status(404)
+          .json({ message: "There was no group located for this id." });
+        return;
       }
+      res.json(dbGroupData);
     })
-      .then((dbGroupData) => {
-        if (!dbGroupData) {
-          res
-            .status(404)
-            .json({ message: "There was no group located for this id." });
-          return;
-        }
-        res.json(dbGroupData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-  router.post('/', (req, res) => {
-    Group.create({
-      group_name: req.body.group_name,
-    })
-      .then(dbGroupData => res.json(dbGroupData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
+router.post("/", (req, res) => {
+  Group.create({
+    group_name: req.body.group_name,
+  })
+    .then((dbGroupData) => res.json(dbGroupData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
