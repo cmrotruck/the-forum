03371589b0d7c177c61ef6ed8d24468e2f0b8fdd
Debbie Hall <debleehall15@gmail.com>
2022-02-15 21:58:49 -0500

    playing with the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
+const res = require("express/lib/response");
 const sequelize = require("../config/connection");
 const { Post, User, Comment, Group } = require("../models");
 
 // handlebars set up
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
+  Group.findAll()
+    .then(dbGroupData => {
+      const groups = dbGroupData.map(group => group.get({ plain: true }));
+
+      res.render('homepage', { groups });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  });
+
 
 module.exports = router;
