ac956160c45fc62d1c6e08731f420a537e67f1c0
cmrotruck <crotruck@gmail.com>
2022-02-20 17:51:13 -0500

    fixed formatting on dates

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,31 +1,33 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const post_title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('textarea[name="post-text"]').value;
-    const post_image = document.querySelector('input[name="post-image"]').value;
-    const group_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+  event.preventDefault();
 
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        group_id,
-        post_title,
-        post_text,
-        post_image
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+  const post_title = document.querySelector('input[name="post-title"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
+  const post_image = document.querySelector('input[name="post-image"]').value;
+  const group_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
 
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      group_id,
+      post_title,
+      post_text,
+      post_image,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('#post-button').addEventListener('click', newFormHandler);
+}
+
+document
+  .querySelector("#post-button")
+  .addEventListener("click", newFormHandler);
